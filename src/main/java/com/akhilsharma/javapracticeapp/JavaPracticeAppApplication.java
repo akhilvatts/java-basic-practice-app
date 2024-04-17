fccc84ae727ab5547e0df12294a3b4752b1cb31e
Akhil Sharma <desire.akhil@gmail.com>
2024-04-17 14:05:13 +0530

    sample basic app

---
+++ src/main/java/com/akhilsharma/javapracticeapp/JavaPracticeAppApplication.java
@@ -0,0 +1,14 @@
+package com.akhilsharma.javapracticeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPracticeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPracticeAppApplication.class, args);
+		System.out.println("Hello, This is a sample application!");
+	}
+
+}
---
+++ src/test/java/com/akhilsharma/javapracticeapp/JavaPracticeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akhilsharma.javapracticeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPracticeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
